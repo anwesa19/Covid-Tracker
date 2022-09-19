fc9eab4f48ea88dfdfc93f7aa4be25a9fc8896d5
anwesa19 <anwesapati19@gmail.com>
2022-09-20 01:37:33 +0530

    Updates

---
+++ precache-manifest.ff6ecd98a287de4ec2fde72bef242827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292a990ccb7c699c9cfcb203c59fdaef",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "33f78f1918d6f70a1dcb",
+    "url": "/Covid-Tracker/static/css/main.410e4ad9.chunk.css"
+  },
+  {
+    "revision": "bb2d651c7338dfa3b9ba",
+    "url": "/Covid-Tracker/static/js/2.aa283780.chunk.js"
+  },
+  {
+    "revision": "a31a65f80ffb624a646de165a7dbfb2d",
+    "url": "/Covid-Tracker/static/js/2.aa283780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f78f1918d6f70a1dcb",
+    "url": "/Covid-Tracker/static/js/main.459700e2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Covid-Tracker/static/js/main.459700e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6689a7ca99682ced4777",
+    "url": "/Covid-Tracker/static/js/runtime-main.45c1b83c.js"
+  }
+]);
